369e59a84d3d70acfc2cb405fc2e7439f91855c1
ahmed gaafer <ahmeds.gaafer@gmail.com>
2023-11-18 01:32:04 +0200

    new end points

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 10000;
 const hash = {};
 
+app.use(cors());
+app.use(express.json());
+
 const PASS = process.env.HASH_KEY;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
-app.get("/v1/hash", (req, res) => {
+app.post('/v1/validate/', (req, res) => {
+	const { password } = req.body;
+
+	res.status(200).send({ status: password === PASS });
+});
+
+app.get('/v1/hash', (req, res) => {
 	const { key, value, pass } = req.query;
 
 	if (pass !== PASS) {
-		return res.status(401).send("Unauthorized");
+		return res.status(401).send('Unauthorized');
 	}
 
 	hash[key] = value;
-	res.status(201).send(
-		`Saved a new hash with key: ${key}  and value: ${value}`
-	);
+	res
+		.status(201)
+		.send(`Saved a new hash with key: ${key}  and value: ${value}`);
 });
 
-app.get("/v1/hash/:key", (req, res) => {
+app.get('/v1/hash/all', (req, res) => {
+	res.status(200).send({ hash });
+});
+
+app.get('/v1/hash/:key', (req, res) => {
 	const { key } = req.params;
 	const value = hash[key];
 	if (!value) {
